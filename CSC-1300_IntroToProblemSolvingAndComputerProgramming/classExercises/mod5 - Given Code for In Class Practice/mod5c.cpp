0109a57e8de047f966199e5cc2c0e838e49644e6
Matthew Love <matthewtglove@gmail.com>
2023-09-15 10:29:46 -0500

    WIP module 5c

--- CSC-1300_IntroToProblemSolvingAndComputerProgramming/classExercises/mod5 - Given Code for In Class Practice/mod5c.cpp
+++ CSC-1300_IntroToProblemSolvingAndComputerProgramming/classExercises/mod5 - Given Code for In Class Practice/mod5c.cpp
@@ -1,7 +1,7 @@
 /*
 	Title:    mod5c.cpp
-	Author:   
-	Date:     
+	Author: Matthew Love
+	Date: 2023-09-15, Fri
 	Purpose:  Practice for loops (in-class practice)
 */
 
@@ -10,17 +10,44 @@
 #include <ctime>
 using namespace std;
 
+int diceRoll()
+{
+	// seed the random number generator
+	srand(time(0));
+
+	return (rand() % 6);
+}
+
+// Function for asking the user if they'd like to roll again
+bool askRunAgain(string prompt = "Would you like to run this program again? (y/n) ")
+{
+	while (true)
+	{
+		cout << prompt;
+
+		// Parses user input - repeats only the first character they input
+		string input;
+		getline(cin, input);
+		char runAgainResponse = tolower(input[0]);
+
+		if (runAgainResponse == 'y')
+		{
+			return true;
+			break;
+		}
+		else if (runAgainResponse == 'n')
+		{
+			return false;
+			break;
+		}
+	}
+}
+
 int main()
 {
-	int rnum = 0; 	//rnum holds the random number generated
-	int total;  	//total for the particular ability
-	int oddRoll;	//holds the number of times an ODD roll occurred
-	int rollNum;	//Number of times dice has been rolled for this ability
-	
-	
-	//seed the random number generator
-	srand (time(0));
-	
+	int total;		  // total for the particular ability
+	int oddRollCount; // holds the number of times an ODD roll occurred
+
 	cout << "\nDnD Stat Roll\n";
 	cout << "1 - Strength\n";
 	cout << "2 - Dexterity\n";
@@ -28,35 +55,45 @@ int main()
 	cout << "4 - Intelligence\n";
 	cout << "5 - Wisdom\n";
 	cout << "6 - Charisma\n";
-	
+
 	cout << "\nFor your character stats, I am rolling a"
 			" 6-sided die until an ODD dice roll \noccurs more "
 			"than once OR the total of all dice rolled so far is equal\n"
 			"to or exceeds 20, whichever comes first.\n\nIf the total "
 			"exceeds 20, then the last dice roll is removed.\n\n"
 			"I will then print the result for each ability score.\n\n";
-	
-	//ADD ALL YOUR CODE BELOW 
-	//MAKE SURE YOU HAVE AT LEAST ONE FOR LOOP AND TWO LOOPS IN TOTAL.  
-	//USE THE PROVIDED VARIABLES THAT HAVE BEEN DEFINED
-	for (int i = 1; i <= 6; i++)
+
+	// ADD ALL YOUR CODE BELOW
+	// MAKE SURE YOU HAVE AT LEAST ONE FOR LOOP AND TWO LOOPS IN TOTAL.
+	// USE THE PROVIDED VARIABLES THAT HAVE BEEN DEFINED
+	while (true)
 	{
-		total = 0;
-		oddRoll = 0;
-		rollNum = 1;
-		while(total < 20 && oddRoll < 2)
+		bool abilityConditionMet = false;
+		int abilityIndex = 1;
+		for (int rollNum = 1; !abilityConditionMet; rollNum++)
 		{
-			rnum = (rand() % 6)+1;
-			cout << "roll " << rollNum << ": " << rnum << endl;
-			if(rnum % 2 != 0)
-				oddRoll++;
-			total += rnum;
-			rollNum++;
+			int roll = diceRoll();
+			cout << "roll " << rollNum << ": " << roll << endl;
+			oddRollCount++;
+			total += roll;
+
+			(oddRollCount > 1 || total >= 20) ? abilityConditionMet = true : abilityConditionMet = false;
+
+			if (abilityConditionMet)
+			{
+				cout << ">>>>> Ability " << abilityIndex << " Score is " << total << endl;
+				cout << abilityIndex++;
+			}
 		}
-		if(total > 20)
-			total = total - rnum;
-		cout << "***** Ability " << i << " score is " << total << endl;
+		if (askRunAgain("Roll again? (y/n) "))
+		{
+			continue;
+		}
+		else
+		{
+			break;
+		}
 	}
-	cout << endl;
+
 	return 0;
 }
