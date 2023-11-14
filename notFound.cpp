d8be06763530f704167d72c9c14975021788d290
Matthew Love <matthewtglove@gmail.com>
2023-11-14 16:05:24 -0600

    Program with Kendal

---
+++ notFound.cpp
@@ -0,0 +1,20 @@
+/*
+    Title: notFound.cpp
+    Author: Kendal and Matthew
+    Date: 2023-11-06, Mon
+    Purpose: Run ads
+*/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int numberAds = 200;
+
+    for (int i = 0; i < numberAds; i++)
+    {
+        cout << i * 2387 << " Buy Sunbeam Bread. Baker say send me an energy that I make a Sunbeam bread the best better better better than I make it make it ring the bell." << endl;
+    }
+
+    return 0;
+}
