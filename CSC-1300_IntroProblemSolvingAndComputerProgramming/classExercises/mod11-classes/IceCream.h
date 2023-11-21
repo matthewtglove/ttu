ce1b44409c44f9b3d6094a8776a7c28101293ad8
Matthew Love <matthewtglove@gmail.com>
2023-11-20 19:32:18 -0600

    Finished

---
+++ CSC-1300_IntroProblemSolvingAndComputerProgramming/classExercises/mod11-classes/IceCream.h
@@ -0,0 +1,25 @@
+/*
+    Title: IceCream.h
+    Author: Matthew Love
+    Date: 2023-11-17, Fri
+    Purpose: Using classes
+*/
+
+#ifndef ICECREAM_H
+#define ICECREAM_H
+
+#include <string>
+
+class IceCream
+{
+private:
+    std::string cone;
+    std::string flavor;
+    std::string topping;
+    int price;
+
+public:
+    // Some sort of function
+};
+
+#endif
