36729ebba17764822654b5ac2493bf49e03262f8
Matthew Love <matthewtglove@gmail.com>
2024-02-20 00:08:00 -0600

    Lab 4 cont

---
+++ CSC-1310_DataStructuresAndAlgorithms/labs/lab4-linkedLIsts/List.h
@@ -0,0 +1,22 @@
+/*
+    Title: Lab 4
+    Author: Matthew Love
+    Date: 2024-02-19, Mon
+    Purpose: Creating a linked list
+*/
+
+// TODO
+// - Look at example linked  list file
+
+#ifndef LIST_H
+#define LIST_H
+
+struct ListNode
+{
+};
+
+class List
+{
+};
+
+#endif
