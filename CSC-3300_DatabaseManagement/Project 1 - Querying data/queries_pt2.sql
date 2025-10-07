ed5d7b9931c9b10339862fa52a78f834a25c94c9
Matthew Love <matthewtglove@gmail.com>
2025-10-07 00:34:12 -0500

    Finished

--- CSC-3300_DatabaseManagement/Project 1 - Querying data/queries_pt2.sql
+++ CSC-3300_DatabaseManagement/Project 1 - Querying data/queries_pt2.sql
@@ -24,7 +24,7 @@ SELECT
 	c.title AS CourseTitle,
 	s.semester AS Semester,
 	s.year AS Year,
-	COUNT(t.ID)
+	COUNT(t.ID) AS Instructor
 FROM teaches t
 JOIN course c ON t.course_id = c.course_id
 JOIN section s ON t.course_id = s.course_id AND t.sec_id = s.sec_id AND t.year = s.year AND t.semester = s.semester
@@ -35,7 +35,7 @@ HAVING COUNT(t.ID) >= 2
 SELECT
 	s.ID AS StudentID,
 	s.name AS StudentName,
-	COUNT(i.id) AS InstructorCount
+	COUNT(DISTINCT i.id) AS InstructorCount
 FROM student s
 JOIN takes tk ON s.ID = tk.ID
 JOIN teaches te ON tk.course_id = te.course_id AND tk.sec_id = te.sec_id AND tk.year = te.year AND tk.semester = te.semester
@@ -52,6 +52,7 @@ JOIN takes tk ON s.ID = tk.ID
 JOIN teaches te ON tk.course_id = te.course_id
 JOIN instructor i ON te.ID = i.id
 JOIN advisor a ON s.ID = a.s_ID
+WHERE a.i_ID = te.ID
 
 -- 6. Retrieve the IDs and names of students, along with information about courses (course IDs) they have taken more than once.
 SELECT 
@@ -64,7 +65,10 @@ GROUP BY s.ID, s.name, tk.course_id
 HAVING COUNT(tk.course_id) > 1
 
 -- 7. Retrieve the credit hours each instructor taught in the Fall semester of 2009. Exclude instructors who did not teach during that semester.
-SELECT i.id, i.name, SUM(c.credits)
+SELECT 
+	i.id, 
+	i.name, 
+	SUM(c.credits)
 FROM instructor i
 JOIN teaches te ON i.id = te.ID
 JOIN course c ON te.course_id = c.course_id
@@ -73,10 +77,33 @@ AND te.semester = 'Fall'
 GROUP BY i.id
 
 -- 8. Include the course ID, title, and department name for each course, along with the corresponding prerequisite course ID, title, and department name.
-SELECT c.course_id AS Course, c.title AS Title, c.dept_name AS Department, p.prereq_id AS PrereqID
+-- TODO: Fix department always returning null
+SELECT 
+	c.course_id AS Course, 
+	c.title AS Title, 
+	c.dept_name AS Department, 
+	p.prereq_id AS PrereqID,
+	c2.title AS PrereqName,
+	c2.dept_name AS PrereqDept
 FROM course c
 LEFT JOIN prereq p ON c.course_id = p.course_id
+LEFT JOIN course c2 ON p.prereq_id = c2.course_id 
 
 -- 9. For each department (department name) the average number of credits they offer per year.
+SELECT 
+	d.dept_name,
+	AVG(c.credits) AS AvgCredits
+FROM department d
+JOIN course c ON d.dept_name = c.dept_name
+GROUP BY d.dept_name
 
 -- 10. Building and room number of classrooms with the smallest capacity.
+SELECT
+	c.building,
+	c.room_number,
+	c.capacity
+FROM classroom c
+WHERE capacity = (
+	SELECT MIN(capacity)
+)
+
