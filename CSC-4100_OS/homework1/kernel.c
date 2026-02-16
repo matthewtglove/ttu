834da133de28f2127d72ecd6b9afe8d07e15bc9e
Matthew Love <matthewtglove@gmail.com>
2026-02-15 21:46:51 -0600

    Complete kernel main file

--- CSC-4100_OS/homework1/kernel.c
+++ CSC-4100_OS/homework1/kernel.c
@@ -1,16 +1,12 @@
 #include "libos.h"
 
-// Clear a rectangular area of the screen
 int clear_scr(int start_row, int start_col, int width, int height) {
-    // check if the parameters are valid
-    if (start_row < 0 || start_col < 0 || width < 0 || height < 0) {
-        return 1;
-    }
+    // Ensure we don't go out of bounds
     if (start_row + height > term_txtheight() || start_col + width > term_txtwidth()) {
         return 1;
     }
 
-    // loop through the rows and columns of the rectangular area and print a space character
+    // Fill the area with spaces
     for (int r = start_row; r < start_row + height; r++) {
         for (int c = start_col; c < start_col + width; c++) {
             putc_to(r, c, ' ');
@@ -20,20 +16,21 @@ int clear_scr(int start_row, int start_col, int width, int height) {
 }
 
 int main(void) {
-    clear_scr(0, 0, term_txtwidth(), term_txtheight());
-
-    // call the box function with the start row, start column, end row, and end column of the box
+    // Requirements: srow=21, scol=49, erow=27, ecol=79
     unsigned int srow = 21;
     unsigned int scol = 49;
     unsigned int erow = 27;
     unsigned int ecol = 79;
+
+    int width = (ecol - scol) + 1;
+    int height = (erow - srow) + 1;
+
+    clear_scr(srow, scol, width, height);
     box(srow, scol, erow, ecol);
+    print_to(24, 59, "Hello world");
 
-    // call print_to with the row and column, and string to print
-    int row = 24;
-    int col = 59;
-    char *string = "Hello world";
-    print_to(row, col, string);
-
+    while (1) {
+        // Inifinite loop
+    };
     return 0;
 }
