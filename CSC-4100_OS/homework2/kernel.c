41a22b1862f4abaa001afb8cadab364261b31734
Matthew Love <matthewtglove@gmail.com>
2026-03-14 14:56:40 -0500

    Comment cleanup

--- CSC-4100_OS/homework2/kernel.c
+++ CSC-4100_OS/homework2/kernel.c
@@ -6,7 +6,6 @@ int clear_scr(int start_row, int start_col, int width, int height) {
     if (start_row + height > term_txtheight() || start_col + width > term_txtwidth()) {
         return 1;
     }
-
     // Fill the area with spaces
     for (int r = start_row; r < start_row + height; r++) {
         for (int c = start_col; c < start_col + width; c++) {
@@ -19,17 +18,15 @@ int clear_scr(int start_row, int start_col, int width, int height) {
 int main(void) {
     int width = term_txtwidth();
     int height = term_txtheight();
+
+    // Start from a clean screen and show a simple title.
     clear_scr(0, 0, width, height);
     print_to(0, 0, "Running processes");
 
-    // → call q_init(Ready_q)
-    // initialize Ready queue data structure. The prototype for q_init is: void q_init(Queue_t *)
-    // The first arrow line is "call q_init(Ready_q)" so that your ready queue that will order the execution of the processes is initialized properly.  This function, which you will write in C/C++, will set the head and tail pointers of Ready_q to NULL (or zero).
+    // Initialize the ready queue before any processes are created.
     q_init(Ready_q);
 
-    // → set retval to the return value of spawn_process(p1)
-    // The second arrow line is a call to spawn_process(p1), which will set up an initial context for the process, allocate its PCB, and put a pointer to the PCB in the ready queue.  The argument p1 is a pointer to a function.  This will be the code that the new process will execute.
-    // The prototype of sqawn_process is: int spawn_process(int (*pfun)())
+    // Create the four cooperative processes and report any startup failure.
     int retval = spawn_process(p1);
     if (retval != 0) {
         print_to(1, 0, "Error spawning process p1");
@@ -47,10 +44,7 @@ int main(void) {
         print_to(4, 0, "Error spawning process p4");
     }
 
-    // → asm volatile("b restore_context");
-    // dispatch the current Running process This restores the context of the process at which Running points, and then jumps to its next instruction.
-    // Note: retore_context is a label in assembler.  You must declare it as .global in the assembly file to use it here.
-    // The next line with an arrow is an inlined assembly language instruction (only use inline assembly this once; all you other assembly code should be in an assembly file). This instruction jumps to assembly code that dispatches the process indicated by the variable Running by restoring the process' context and jumping to its first instruction, which completes system startup. Later in this assignment description, you will be shown exactly what actual code you need to have for spawn_process() and dispatch_select()and restore_context.
+    // Hand off to the assembly restore path, which selects the first ready process, restores its saved context, and begins executing it.
     asm volatile("b restore_context");
 
     // Question to professor: Should we ever use this safeguard?
--- CSC-4100_OS/homework2/process.c
+++ CSC-4100_OS/homework2/process.c
@@ -6,7 +6,7 @@ PCB_Q_t *Ready_q = &Ready_q_storage;
 PCB_t *Running = 0;
 uint32_t next_pid = 1;
 
-// Static pool allocators (bare-metal: no malloc)
+// Fixed-size pools are used because this bare-metal environment has no malloc.
 #define MAX_PROCS 8
 #define STACK_WORDS 1024
 
@@ -32,50 +32,35 @@ PCB_t *alloc_pcb() {
 }
 
 int spawn_process(int (*code_address)()) {
-    // code_address is the pointer to the function that contains the process code
-
-    // set stackptr to return value of calling alloc_stack() to allocate a stack that is 1024 uint64_t in size.
-    // check to make sure stackptr is not null (if it is, return -1)
+    // Allocate a 1024-word stack for the new process.
     uint64_t *stackptr = alloc_stack(1024);
     if (stackptr == 0) {
         return -1;
     }
 
-    // First, set up the stack, pretend sp is a uint64_t *
-
-    // set sp to stackptr + 1024 to put sp at the bottom of the stack
-    // Note that if stackptr is a pointer to a uint64_t, then stackptr+1024 will point to the right place
+    // Start at the top of the stack and build the initial saved-register frame.
     uint64_t *sp = stackptr + 1024;
 
-    // create context that will set gen purpose regs to 0
-    // loop i from 0 to 32 (inclusive)
-    //     decrement sp
-    // 	set the uint64_t at which sp points to 0
+    // Zero x0-x30 plus the saved PSTATE slot expected by restore_context.
     for (int i = 0; i <= 32; i++) {
         sp--;
         *sp = 0;
     }
 
-    // set the value at which sp+30 points to code_address
-    // type cast code_address to a uint64_t so that the compiler does not complain)
+    // Place the entry function where x30/LR will be restored from.
     *(sp + 30) = (uint64_t)code_address;
 
-    // Note: you will have to create the pcb struct for the following code
-    // set pcb to the return value of alloc_pcb() to allocate a pcb for the process
+    // Allocate and initialize the PCB for this process.
     PCB_t *pcb = alloc_pcb();
-    // check the pcb and if is null then return -1
     if (pcb == 0) {
         return -1;
     }
 
-    // set the pcb's sp member to sp.
     pcb->sp = (uint64_t)sp;
-
-    // set the pcb's pid member to next_pid  //Note that you can keep track of the next pid using a global variable
     pcb->pid = next_pid++;
     pcb->next = 0;
 
-    // enqueue the pcb onto the Ready_q // Ready_q should be a global
+    // New processes enter the ready queue in FIFO order.
     q_enqueue(Ready_q, pcb);
 
     /* no errors occured */
@@ -83,17 +68,20 @@ int spawn_process(int (*code_address)()) {
 }
 
 void dispatch_select(void) {
+    // Pick the next ready process to become Running.
     Running = q_dequeue(Ready_q);
 }
 
 extern void restore_context(void);
 
 void dispatch_restore(void) {
+    // Convenience wrapper around the assembly restore label.
     asm volatile("b restore_context");
     __builtin_unreachable();
 }
 
 static int is_prime(unsigned long long n) {
+    // Simple brute-force prime test used by each process.
     if (n < 2ULL) {
         return 0;
     }
@@ -103,12 +91,14 @@ static int is_prime(unsigned long long n) {
         if ((n % divisor) == 0ULL) {
             return 0;
         }
+        // Yield during the long-running prime test so other processes can run.
         dispatch();
     }
 
     return 1;
 }
 
+// Draw the process window and show its initial value. Then loop forever, updating the value and yielding to other processes.
 int p1(void) {
     static char message[] = "Process 1: 0";
     unsigned long long num = 1ULL;
